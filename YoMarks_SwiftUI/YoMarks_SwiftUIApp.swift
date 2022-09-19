dad1b56ab62169c9898ac318e1a3c3ea1f821dae
Maksim Botalov <dev.botalov@gmail.com>
2022-09-19 17:40:14 +0500

    Initial Commit

---
+++ YoMarks_SwiftUI/YoMarks_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoMarks_SwiftUIApp.swift
+//  YoMarks_SwiftUI
+//
+//  Created by Максим Боталов on 19.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct YoMarks_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
